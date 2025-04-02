986e9f4edcad37612feb14f057a798e932c94702
EzeG92 <gim24001@byui.edu>
2025-04-02 15:26:11 -0300

    header and footer dynamic

---
+++ src/js/utils.js
@@ -0,0 +1,21 @@
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.insertAdjacentHTML("afterbegin", template);
+    callback && callback(data);
+}
+
+export async function loadTemplate(path) {
+    const response = await fetch(path);
+    const template = await response.text();
+    return template;
+}
+
+export async function loadHeaderFooter() {
+    const headerTemplate = await loadTemplate("../partials/header.html");
+    const footerTemplate = await loadTemplate("../partials/footer.html");
+    const headerElement = document.querySelector("#main-header");
+    const footerElement = document.querySelector("#main-footer");
+    
+    renderWithTemplate(headerTemplate, headerElement);
+    renderWithTemplate(footerTemplate, footerElement);
+}

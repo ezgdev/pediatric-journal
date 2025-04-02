dcd58587d90a0a97ecffcddde26714e110a48066
EzeG92 <gim24001@byui.edu>
2025-04-02 13:47:30 -0300

    fixed vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+const { defineConfig } = require("vite");
 
 export default defineConfig({
   root: 'src',
+  build: {
+    outDir: '../dist', // Establecer la carpeta de salida en el directorio raíz
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
   server: {
     port: 3000,
   },

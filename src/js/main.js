f4894bd57e8b6a7a694d1bf941e44f0d064f01f2
EzeG92 <gim24001@byui.edu>
2025-04-02 17:00:39 -0300

    hamButton

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import { loadHeaderFooter } from "./utils.mjs"; 
 
 loadHeaderFooter();
+
+
+//add content to footer
+const year = new Date().getFullYear();
+const copyrightText = `&copy; ${year} Ezequiel Gimenez - Rosario, Argentina.`;
+const footerElement = document.getElementById('main-footer');
+footerElement.innerHTML = copyrightText;
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -17,4 +17,16 @@ export async function loadHeaderFooter() {
 
     renderWithTemplate(headerTemplate, headerElement);
     renderWithTemplate(footerTemplate, footerElement);
+
+    initializeMenuToggle();
+}
+
+export function initializeMenuToggle() {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
 }
